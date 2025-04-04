d36e02efebd2639c9fe952140082c80609be2e80
Diamondy4 <diamondy4@hotmail.com>
2025-04-04 12:29:31 +0300

    feat: aerospike 7.0.4 + overlay

--- flake.nix
+++ flake.nix
@@ -2,48 +2,58 @@
   description = "The Aerospike C client provides a C interface for interacting with the Aerospike Database.";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs";
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
     flake-parts.url = "github:hercules-ci/flake-parts";
-    flake-compat = {
-      url = "github:edolstra/flake-compat";
-      flake = false;
-    };
     aerospike-client-c = {
       url = "git+ssh://git@github.com/aerospike/aerospike-client-c";
       type = "git";
       submodules = true;
       flake = false;
-      rev = "bdd08709ed5be34b634aa69682144986d1ae09b4";
+      rev = "354a1283c8179cf3dce66c622dad7a77a804e6a2";
     };
   };
 
-  outputs = inputs@{ nixpkgs, flake-parts, ... }:
+  outputs =
+    inputs@{ nixpkgs, flake-parts, ... }:
     flake-parts.lib.mkFlake { inherit inputs; } {
-      #mipsel-linux system somehow fails 'nix flake check'
-      systems = nixpkgs.lib.lists.remove "mipsel-linux" nixpkgs.lib.systems.flakeExposed;
-      perSystem = { self', config, pkgs, lib, system, ... }:
-        let
-          aerospike-c-client = pkgs.stdenv.mkDerivation {
-            pname = "aerospike-c-client";
-            version = "6.3.1";
+      systems = nixpkgs.lib.systems.flakeExposed;
+      imports = [
+        inputs.flake-parts.flakeModules.easyOverlay
+      ];
+      perSystem =
+        {
+          self',
+          config,
+          pkgs,
+          lib,
+          system,
+          ...
+        }:
+        {
+          packages.default = pkgs.stdenv.mkDerivation {
+            pname = "aerospike-client-c";
+            version = "7.0.4";
             src = inputs.aerospike-client-c;
             buildInputs = with pkgs; [
               openssl
               zlib
               lua
             ];
+            patchPhase = ''
+              patchShebangs *
+              substituteInPlace modules/lua/makefile \
+                --replace-fail "AR= ar rc" "override AR= ar rc"
+            '';
             installPhase = ''
               runHook preInstall
               shopt -s globstar
               mkdir -p $out
               cp -r ./**/include $out
               cp -r ./**/lib $out
+              cp -r ./**/obj $out
               runHook postInstall
             '';
           };
-        in
-        {
-          packages.default = aerospike-c-client;
         };
     };
 }
